9b66b151c11a03a2dada8c3d5dc2bbe3cdf0e101
Tomaz Vieira <tom.fogaca@gmail.com>
2023-08-20 16:37:16 +0200

    [WIP] Combine filters has actual fields now

--- src/feature_extractor_pipeline/kernel/combined_filters.rs
+++ src/feature_extractor_pipeline/kernel/combined_filters.rs
@@ -1,15 +1,20 @@
+use wgpu::Extent3d;
+
 use crate::feature_extractor_pipeline::kernel::{SampleVar, CenterOffset, AccumulatorVar};
 
 use super::gaussian_blur::GaussianBlur;
 
-pub struct CombinedFilters{}
-impl CombinedFilters{
-    fn produce_shader(kernels: &[GaussianBlur]){
+pub struct CombinedFilters<'kernels>{
+    pub kernels: &'kernels [GaussianBlur],
+    pub input_extent: Extent3d,
+}
+impl CombinedFilters<'_>{
+    pub fn produce_shader(&self) -> String{
         let mut shader_code = String::with_capacity(1024 * 1024);
 
-        let accumulator_vars: Vec<AccumulatorVar> = kernels.iter().map(|kernel| kernel.make_accumulator_var()).collect();
+        let accumulator_vars: Vec<AccumulatorVar> = self.kernels.iter().map(|kernel| kernel.make_accumulator_var()).collect();
         let sample_var = SampleVar(format!("sample"));
-        let radius: i8 = kernels.iter().map(|k| k.max_offset).max().unwrap().try_into().unwrap();
+        let radius: i8 = self.kernels.iter().map(|k| k.max_offset).max().unwrap().try_into().unwrap();
 
         accumulator_vars.iter().for_each(|acc_var|{
             shader_code += &format!("let {acc_var}: f32 = 0.0;\n");
@@ -19,8 +24,11 @@ impl CombinedFilters{
         for y in -radius..=radius{
             for x in -radius..=radius{
                 let center_offset = CenterOffset{x, y, z: 0};
-                shader_code += &format!("{sample_var} = textureLoad(some_input, vec2i({x}, {y}), 0);\n");
-                for (kernel_index, kernel) in kernels.iter().enumerate(){
+                //FIXME: use a sampler instead
+                let clamped_x: i32 = (i32::from(x)).clamp(0, (self.input_extent.width - 1).try_into().unwrap());
+                let clamped_y: i32 = (i32::from(y)).clamp(0, (self.input_extent.height - 1).try_into().unwrap());
+                shader_code += &format!("{sample_var} = textureLoad(some_input, vec2i({clamped_x}, {clamped_y}), 0);\n");
+                for (kernel_index, kernel) in self.kernels.iter().enumerate(){
                     match kernel.make_accumulate_statement(&accumulator_vars[kernel_index], &sample_var, &center_offset){
                         None => continue,
                         Some(shader_line) => {
@@ -31,15 +39,18 @@ impl CombinedFilters{
                 }
             }
         }
-
-        println!("Shader code:\n{shader_code}");
+        return shader_code
     }
 }
 
 #[test]
 fn produce_shader(){
-    CombinedFilters::produce_shader(&[
-        GaussianBlur{sigma: 0.3, max_offset: 3},
-        GaussianBlur{sigma: 0.7, max_offset: 3},
-    ])
+    let shader_code = CombinedFilters{
+        kernels: &[
+            GaussianBlur{sigma: 0.3, max_offset: 3},
+            GaussianBlur{sigma: 0.7, max_offset: 3},
+        ],
+        input_extent: Extent3d { width: 3, height: 3, depth_or_array_layers: 1 }
+    }.produce_shader();
+    println!("Shader code:\n{shader_code}");
 }
