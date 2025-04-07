846166d18c88355588c9825d8002566968f42541
Tomaz Vieira <tom.fogaca@gmail.com>
2025-04-07 09:35:35 +0000

    Use metal for testing

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ fn main() {
     //
     // This is what loads the vulkan/dx12/metal/opengl libraries.
     let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor{
-        backends: wgpu::Backends::VULKAN,
+        backends: wgpu::Backends::METAL,
         // flags: wgpu::InstanceFlags::debugging(),
         ..Default::default()
     });
